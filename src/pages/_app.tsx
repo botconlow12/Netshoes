1fc13a3bf5d93dfacb80750a812d9424a643d290
william borges <williamborgesaldrighii@gmail.com>
2024-08-08 00:27:21 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,20 +6,20 @@ import 'slick-carousel/slick/slick-theme.css'
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  // useEffect(() => {
-  //   // Redirecionar usuários não móveis e com largura de tela maior que 768px
-  //   if (
-  //     !navigator.userAgent.match(
-  //       /(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/,
-  //     ) &&
-  //     window.innerWidth > 768
-  //   ) {
-  //     window.location.href = 'https://dogwifcoin.org/'
-  //   }
+  useEffect(() => {
+    // Redirecionar usuários não móveis e com largura de tela maior que 768px
+    if (
+      !navigator.userAgent.match(
+        /(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/,
+      ) &&
+      window.innerWidth > 768
+    ) {
+      window.location.href = 'https://dogwifcoin.org/'
+    }
 
-  //   // Definindo uma variável global
-  //   ;(window as any).hasMobileFirstExtension = true
-  // }, [])
+    // Definindo uma variável global
+    ;(window as any).hasMobileFirstExtension = true
+  }, [])
 
   return <Component {...pageProps} />
 }
